4f914640559b60250a542a3cb1b33a4f05d21da9
mishean <misheanpeiris@gmail.com>
2023-05-18 00:06:54 -0400

    added in via upload from device

--- sanity/schema.js
+++ sanity/schema.js
@@ -1,8 +1,9 @@
 import blockContent from './schemas/blockContent'
 import post from './schemas/post'
 import author from './schemas/author'
+import webvideo from './schemas/webvideo'
 import video from './schemas/video'
 
 export const schema = {
-  types: [post, author, video, blockContent],
+  types: [post, author, webvideo, video, blockContent],
 }
