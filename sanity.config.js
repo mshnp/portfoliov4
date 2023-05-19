6ec5d577e22ca4182c0d133ddc8163f534e4e7f9
mishean <misheanpeiris@gmail.com>
2023-05-19 17:53:07 -0400

    added video support

--- sanity.config.js
+++ sanity.config.js
@@ -4,9 +4,9 @@
 
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import { muxInput } from 'sanity-plugin-mux-input'
+//import {muxInput} from 'sanity-plugin-mux-input'
 import {deskTool} from 'sanity/desk'
-
+import {media} from 'sanity-plugin-media'
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schema'
@@ -20,7 +20,7 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
-    deskTool(), muxInput(), 
+    deskTool(), media(), //muxInput(), 
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
--- sanity/schema.js
+++ sanity/schema.js
@@ -2,7 +2,8 @@ import blockContent from './schemas/blockContent'
 import post from './schemas/post'
 import author from './schemas/author'
 import webvideo from './schemas/webvideo'
+import newVideo from './schemas/newVideo'
 
 export const schema = {
-  types: [post, author, webvideo, blockContent],
+  types: [post, author, webvideo, newVideo, blockContent],
 }
