ceb8d3d28d35fcef2a566e98e40236ebccff6de4
mishean <misheanpeiris@gmail.com>
2023-05-19 16:28:30 -0400

    yuh

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:["cdn.sanity.io"],
-      }
+  images: {
+      domains: ["cdn.sanity.io"],
+  },
+  swcMinify: true,
+  transpilePackages: ['sanity-plugin-mux-input', 'media-chrome/dist/react'],
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
