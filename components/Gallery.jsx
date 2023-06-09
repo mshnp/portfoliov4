91a677ab9dc2d5bf8ad5ebf9efaff87ae0037fe0
mishean <misheanpeiris@gmail.com>
2023-06-08 20:57:08 -0400

    added in photos for syzl post and styled rich text

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,78 +1,70 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 import { urlForImage } from '@/sanity/lib/image';
 
-const Gallery = ({gallery}) => {
+const Gallery = ({ gallery }) => {
+  const { row1, row2, row3, caption } = gallery;
 
-     const {row1, row2, row3, caption} = gallery
+  const getColumnWidth = (row) => {
+    const imageCount = row?.length;
+    return `w-full md:w-1/${imageCount} p-2`;
+  };
 
-     const getColumnWidth = (row) => {
-        const imageCount = row.length;
-        return `md:w-1/${imageCount} sm:w-full p-2`;
-      };
-    
-      const columnWidth1 = getColumnWidth(row1);
-      const columnWidth2 = row2 ? getColumnWidth(row2) : null;
-      const columnWidth3 = row3 ? getColumnWidth(row3) : null;
+  const columnWidth1 = getColumnWidth(row1);
+  const columnWidth2 = row2 ? getColumnWidth(row2) : null;
+  const columnWidth3 = row3 ? getColumnWidth(row3) : null;
 
-console.log(columnWidth1)
   return (
     <section>
-        
-<div>
-  {row1 && (
-    <div className="flex sm: flex-wrap lg:flex-nowrap bg-yellow-100 p-4 mb-4">
-      {row1.map(image => (
-        <div key={image._id}  className={`${columnWidth1}`}>
-          <Image
-            className="w-full rounded-lg shadow-lg"
-            alt={image.alt}
-            width="9999"
-            height="9999"
-            src={urlForImage(image).url()}
-          />
+      {row1 && (
+        <div className="flex flex-wrap bg-yellow-100 p-4 mb-4 md:flex-nowrap">
+          {row1.map((image) => (
+            <div key={image._id} className={`${columnWidth1}`}>
+              <Image
+                className="w-full rounded-lg shadow-lg"
+                alt={image.alt}
+                width="9999"
+                height="9999"
+                src={urlForImage(image).url()}
+              />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-  )}
-  {row2 && (
-    <div className="flex sm: flex-wrap lg:flex-nowrap bg-yellow-100 p-4 mb-4">
-    {row2.map(image => (
-        <div key={image._id}  className={`${columnWidth2}`}>
-          <Image
-            className="w-full rounded-lg shadow-lg"
-            alt={image.alt}
-            width="1000"
-            height="1000"
-            src={urlForImage(image).url()}
-          />
-        </div>
-      ))}
-    </div>
-  )}
-  {row3 && (
-    <div className="flex sm: flex-wrap lg:flex-nowrap bg-yellow-100 p-4 mb-4">
-    {row3.map(image => (
-        <div key={image._id}  className={`${columnWidth3}`}>
-          <Image
-            className="w-full rounded-lg shadow-lg"
-            alt={image.alt}
-            width="1000"
-            height="1000"
-            src={urlForImage(image).url()}
-          />
-        </div>
-      ))}
-    </div>
-  )}
-</div>
-{caption && 
-<p>
-  {caption}
-</p>
-}
-    </section>
-  )
-}
+      )}
 
-export default Gallery
+      {row2 && (
+        <div className="flex flex-wrap bg-yellow-100 p-4 mb-4 md:flex-nowrap">
+          {row2.map((image) => (
+            <div key={image._id} className={`${columnWidth2}`}>
+              <Image
+                className="w-full rounded-lg shadow-lg"
+                alt={image.alt}
+                width="9999"
+                height="9999"
+                src={urlForImage(image).url()}
+              />
+            </div>
+          ))}
+        </div>
+      )}
+
+      {row3 && (
+        <div className="flex flex-wrap bg-yellow-100 p-4 mb-4 md:flex-nowrap">
+          {row3.map((image) => (
+            <div key={image._id} className={`${columnWidth3}`}>
+              <Image
+                className="w-full rounded-lg shadow-lg"
+                alt={image.alt}
+                width="9999"
+                height="9999"
+                src={urlForImage(image).url()}
+              />
+            </div>
+          ))}
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Gallery;
--- components/RichTextComponents.jsx
+++ components/RichTextComponents.jsx
@@ -4,31 +4,30 @@ import WebVideoDisplay from "./WebVideoDisplay";
 
 export const RichTextComponents = {
   types: {
-
-    myGallery: ({value}) => <Gallery  gallery={value} />,
-    blockVideo: ({ value }) => <VideoDisplay videoData={value} />,
-    blockWebVideo: ({value}) => <WebVideoDisplay media={value}/>,
+    myGallery: ({value}) => <div className="flex justify-center"><Gallery  gallery={value} /></div>,
+    blockVideo: ({ value }) => <div className="flex justify-center"><VideoDisplay videoData={value} /></div>,
+    blockWebVideo: ({value}) => <div className="flex justify-center"><WebVideoDisplay media={value}/></div>,
   },
 
-    list: {
-      bullet: ({ children }) => <ul className="mt-xl">{children}</ul>,
-      number: ({ children }) => <ol className="mt-lg">{children}</ol>,
-    },
+  list: {
+    bullet: ({ children }) => <ul className="list-disc pl-8">{children}</ul>, // adjusted to 8x grid (pl-8 = 32px)
+    number: ({ children }) => <ol className="list-decimal pl-8">{children}</ol>, // adjusted to 8x grid (pl-8 = 32px)
+  },
 
-     block: {
-    // Ex. 1: customizing common block types
-    h1: ({children}) => <h1 className="text-5xl">{children}</h1>,
-    h2: ({children}) => <h2 className="text-4xl">{children}</h2>,
-    h3: ({children}) => <h3 className="text-3xl">{children}</h3>,
-    h4: ({children}) => <h4 className="text-2xl">{children}</h4>,
-    blockquote: ({children}) => <blockquote className="border-l-purple-500">{children}</blockquote>,
-     },
+  block: {
+    h1: ({children}) => <h1 className="text-6xl mt-8 mb-8">{children}</h1>, // larger and with bottom margin for hierarchy
+    h2: ({children}) => <h2 className="text-5xl mt-6 mb-6">{children}</h2>, // slightly smaller and less margin than h1
+    h3: ({children}) => <h3 className="text-4xl mt-6 mb-6">{children}</h3>, // smaller and same margin as h2
+    h4: ({children}) => <h4 className="text-3xl mt-4 mb-4">{children}</h4>, // smaller and less margin than h3
+    h5: ({children}) => <h5 className="text-2xl mt-4 mb-4">{children}</h5>, // smaller and same margin as h4
+    normal: ({children}) => <p className="mb-2">{children}</p>, // smallest margin for normal text
+    blockquote: ({children}) => 
+    <div className="flex justify-center">
+      <blockquote className="border-l-purple-500 mt-4 mb-4 text-center max-w-prose mx-auto">{children}</blockquote>
+    </div>,  },
 
-     marks: {
-    // Ex. 1: custom renderer for the em / italics decorator
+  marks: {
     em: ({children}) => <em className="text-gray-600 font-semibold">{children}</em>,
-
-    // Ex. 2: rendering a custom `link` annotation
     link: ({value, children}) => {
       const target = (value?.href || '').startsWith('http') ? '_blank' : undefined
       return (
