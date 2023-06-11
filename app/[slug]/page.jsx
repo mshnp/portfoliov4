16cbb25895be7597b8b7758ff8d9960a24c13b4d
mishean <misheanpeiris@gmail.com>
2023-06-11 16:13:56 -0400

    added in width scoping feature  for gallery

--- app/[slug]/page.jsx
+++ app/[slug]/page.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import { groq } from 'next-sanity';
 import { client } from '@/sanity/lib/client';
-import Image from 'next/image';
-import { urlForImage } from '@/sanity/lib/image';
 import { PortableText } from '@portabletext/react';
 import { RichTextComponents } from '@/components/RichTextComponents';
-import Gallery from '@/components/Gallery';
-import VideoDisplay from '@/components/VideoDisplay';
 import PostBanner from '@/components/PostBanner';
-import WebVideoDisplay from '@/components/WebVideoDisplay';
+import SummaryInformation from '@/components/SummaryInformation';
 
 const Post = async ({ params: { slug } }) => {
   const query = groq`
@@ -34,7 +30,8 @@ const Post = async ({ params: { slug } }) => {
             url
           }
         },
-        caption
+        caption,
+        scopeToMaxWidth,
       },
       mainImages {
         images[] {
@@ -78,9 +75,9 @@ const Post = async ({ params: { slug } }) => {
   const post = await client.fetch(query, { slug });
 
   return (
-    <article className="p-4 sm:p-8">
-      <PostBanner media={post.bannerimageOrVideo} />
-      <h3 className="text-3xl my-4 text-gray-900 dark:text-white">
+    <article>
+      <PostBanner className="p-4 sm:p-8" media={post.bannerimageOrVideo} />
+      <h3 className="text-3xl my-4 p-4 sm:p-8 text-gray-900 dark:text-white">
         {post.title}
       </h3>
       <div className="text-gray-600 dark:text-gray-400">
@@ -100,78 +97,10 @@ const Post = async ({ params: { slug } }) => {
             ))}
           </div>
         </section>
-        <div className="w-full my-8 bg-gray-200 dark:bg-gray-800">
-  <div className="container mx-auto">
-    <section className="grid grid-cols-1 md:grid-cols-2 lg:flex lg:flex-wrap gap-4">
-      {post.duration && (
-        <article className="p-2 flex-auto">
-          <h3 className="text-lg font-bold text-gray-900 dark:text-white">
-            Duration
-          </h3>
-          <p className="text-gray-600 dark:text-gray-400">
-            {post.duration}
-          </p>
-        </article>
-      )}
-      {post.tools.length > 0 && (
-        <article className="p-4 flex-auto">
-          <h3 className="text-lg font-bold text-gray-900 dark:text-white">
-            Tools
-          </h3>
-          {post.tools.map((tool, index) => (
-            <p
-              key={index}
-              className="text-gray-600 dark:text-gray-400"
-            >
-              {tool}
-            </p>
-          ))}
-        </article>
-      )}
-      {post.deliverables.length > 0 && (
-        <article className="p-4 flex-auto">
-          <h3 className="text-lg font-bold text-gray-900 dark:text-white">
-            Deliverables
-          </h3>
-          {post.deliverables.map((deliverable, index) => (
-            <p
-              key={index}
-              className="text-gray-600 dark:text-gray-400"
-            >
-              {deliverable}
-            </p>
-          ))}
-        </article>
-      )}
-      {post.teamMembers.length > 0 && (
-        <article className="p-4 flex-auto">
-          <h3 className="text-lg font-bold text-gray-900 dark:text-white">
-            Team Members
-          </h3>
-          {post.teamMembers.map((member) => (
-            <a
-              key={member._key}
-              href={member.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="block text-blue-500 hover:text-blue-700 hover:underline"
-            >
-              {member.name}
-            </a>
-          ))}
-        </article>
-      )}
-    </section>
-  </div>
-</div>
-
-
-        <section className="my-8">
-          {post.myGallery && <Gallery gallery={post.myGallery} />}
-        </section>
+        <SummaryInformation post={post}/>
       </div>
-      <section className="my-8">
-        <PortableText value={post.body} components={RichTextComponents} />
+      <section className="my-4 p-4 mx-auto sm:p-8">
+      <PortableText value={post.body} components={RichTextComponents} />
       </section>
     </article>
   );
