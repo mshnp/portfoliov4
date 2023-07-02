5194b011e681f761c536f15985e124cf74abcc49
mishean <misheanpeiris@gmail.com>
2023-07-01 20:31:38 -0400

    added an error.js

---
+++ app/error.jsx
@@ -0,0 +1,14 @@
+'use client'
+ 
+export default function Error({ error, reset }) {
+  return (
+    <div className='flex items-center justify-center text-center h-full -mt-20 sm:-mt-32'>
+
+      <h2>Something went wrong!</h2>
+    
+    </div>
+  )
+}
+
+    
+
