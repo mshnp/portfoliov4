74e11bd05e6173313b105d98c246eedd85aeab5f
mishean <misheanpeiris@gmail.com>
2023-07-02 18:04:18 -0400

    fixed GA4 implementation lmao

--- app/layout.js
+++ app/layout.js
@@ -24,16 +24,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}');
-        `}
+    <Script strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4}`}/>
+    <Script
+      strategy='afterInteractive'
+      id ="google-analytics-script">
+      {
+        `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', '${process.env.NEXT_PUBLIC_GA4}');
+        `
+      }
     </Script>
-    <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4}`}></Script>
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <link
   rel="icon"
