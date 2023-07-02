3c55204d1b006a7933c8e8a427301606f59e3fed
mishean <misheanpeiris@gmail.com>
2023-07-02 16:34:16 -0400

    added google tag manager

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,8 @@ import { Header } from '@/components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
-import { Metadata } from 'next'
+import Script from 'next/script'
+
 
 export const revalidate = 60 // revalidate this page every 60 seconds
 
@@ -23,6 +24,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}');
+        `}
+    </Script>
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <link
   rel="icon"
